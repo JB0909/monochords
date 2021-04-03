6f840fb92ef086978194cff8c0740f162f273805
JB0909 <j.kim@dct21.jp>
2021-04-03 17:40:23 +0900

    [追加]各ポストの一覧ページ コメント投稿後コメント一覧描画をajax化

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,13 @@ class CommentsController < ApplicationController
         @record_comment = Comment.where(record_id: comment_params[:record_id])
         @review_comment = Comment.where(review_id: comment_params[:review_id])
         @micropost_comment = Comment.where(micropost_id: comment_params[:micropost_id])
+        if comment_params[:micropost_id].present?
+            @micropost = Micropost.find(comment_params[:micropost_id])
+        elsif comment_params[:record_id].present?
+            @record = Record.find(comment_params[:record_id])
+        elsif comment_params[:review_id].present?
+            @review = Review.find(comment_params[:review_id])
+        end
     end
 
     private
