d345881f436671d8e4bb3cfcaad57e761c713545
JB0909 <j.kim@dct21.jp>
2021-06-03 23:45:12 +0900

    [修正]Rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,7 +17,7 @@ RSpec.describe User, type: :model do
             it '空欄時エラー発生' do
                 test_user.username = ''
                 test_user.valid?
-                expect(test_user.errors[:username]).to include("can't be blank")
+                expect(test_user.errors[:username]).to include("が入力されていません。")
             end
         end
 
@@ -33,7 +33,7 @@ RSpec.describe User, type: :model do
             it '空欄時エラー発生' do
                 test_user.email = ''
                 test_user.valid?
-                expect(test_user.errors[:email]).to include("can't be blank")  
+                expect(test_user.errors[:email]).to include("が入力されていません。")  
             end
 
             it '一意であること' do
@@ -51,7 +51,7 @@ RSpec.describe User, type: :model do
                 test_user_2.email = 'test1@test.co.jp'
                 test_user_2.save
                 test_user_2.valid?
-                expect(test_user_2.errors[:email]).to include("has already been taken")
+                expect(test_user_2.errors[:email]).to include("は既に使用されています。")
             end
         end
 
@@ -66,7 +66,7 @@ RSpec.describe User, type: :model do
             it '空欄時エラー発生' do
                 test_user.password = ''
                 test_user.valid?
-                expect(test_user.errors[:password]).to include("can't be blank")
+                expect(test_user.errors[:password]).to include("が入力されていません。")
             end
 
             it '6文字以上であること' do
@@ -77,14 +77,14 @@ RSpec.describe User, type: :model do
             it '6文字未満時はエラー発生' do
                 test_user.password = Faker::Lorem.characters(number: 1)
                 test_user.valid?
-                expect(test_user.errors[:password]).to include("is too short (minimum is 6 characters)")  
+                expect(test_user.errors[:password]).to include("は6文字以上に設定して下さい。")  
             end
 
             it 'パスワードが不一致' do
                 test_user.password = 'password_1'
                 test_user.password_confirmation = 'password_2'
                 test_user.valid?
-                expect(test_user.errors[:password_confirmation]).to include("doesn't match Password")  
+                expect(test_user.errors[:password_confirmation]).to include("が内容とあっていません。")  
             end
         end
     end
--- spec/views/static_pages/home.html.erb_spec.rb
+++ spec/views/static_pages/home.html.erb_spec.rb
@@ -1,10 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe "static_pages/home", type: :view do
-  describe 'home.html.erbのテスト' do
-    it 'h1タグ内にHello Worldが表示されるか' do
-      visit root_path
-      expect(page).to have_selector('h1', text:'Hello World')
-    end
-  end
+  pending "add some examples to (or delete) #{__FILE__}"
 end
