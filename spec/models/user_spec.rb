2e73a15e55caa11e31cfacb30165932a76ac111f
JB0909 <j.kim@dct21.jp>
2021-06-04 10:07:42 +0900

    [追記]Rspec user_spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -88,4 +88,128 @@ RSpec.describe User, type: :model do
             end
         end
     end
+
+    describe 'アソシエーションのテスト' do
+        let(:association) do
+            described_class.reflect_on_association(target)
+        end
+
+        context 'Micropostモデルとの関係' do
+            let(:target) { :microposts }
+            it '1:Nとなっている' do
+                expect(association.macro).to eq :has_many
+            end
+        end
+
+        context 'Likeモデルとの関係' do
+            let(:target) { :likes }
+            it '1:Nとなっている' do
+                expect(association.macro).to eq :has_many
+            end
+        end
+
+        context 'Record_likeモデルとの関係' do
+            let(:target) { :record_likes }
+            it '1:Nとなっている' do
+                expect(association.macro).to eq :has_many
+            end
+        end
+
+        context 'Review_likeモデルとの関係' do
+            let(:target) { :review_likes }
+            it '1:Nとなっている' do
+                expect(association.macro).to eq :has_many
+            end
+        end
+
+        context 'Following_relationshipモデルとの関係' do
+            let(:target) { :following_relationships }
+            it '1:Nとなっている' do
+                expect(association.macro).to eq :has_many
+            end
+            it '結合するモデルのクラスはRelationship' do
+                expect(association.class_name).to eq 'Relationship'
+            end
+        end
+
+        context 'Followingモデルとの関係' do
+            let(:target) { :following }
+            it '1:Nとなっている' do
+                expect(association.macro).to eq :has_many
+            end
+            it '結合するモデルのクラスはUser(Follower)' do
+                expect(association.class_name).to eq 'User'
+            end
+        end
+
+        context 'Follower_relationshipsモデルとの関係' do
+            let(:target) { :follower_relationships }
+            it '1:Nとなっている' do
+                expect(association.macro).to eq :has_many
+            end
+            it '結合するモデルのクラスはRelationship' do
+                expect(association.class_name).to eq 'Relationship'
+            end
+        end
+
+        context 'Followerモデルとの関係' do
+            let(:target) { :followers }
+            it '1:Nとなっている' do
+                expect(association.macro).to eq :has_many
+            end
+            it '結合するモデルのクラスはUser(Following)' do
+                expect(association.class_name).to eq 'User'
+            end
+        end
+
+        context 'Reviewモデルとの関係' do
+            let(:target) { :reviews }
+            it '1:Nとなっている' do
+                expect(association.macro).to eq :has_many
+            end
+        end
+
+        context 'Wisheモデルとの関係' do
+            let(:target) { :wishes }
+            it '1:Nとなっている' do
+                expect(association.macro).to eq :has_many
+            end
+        end
+
+        context 'Practicingモデルとの関係' do
+            let(:target) { :practicings }
+            it '1:Nとなっている' do
+                expect(association.macro).to eq :has_many
+            end
+        end
+
+        context 'Practicedモデルとの関係' do
+            let(:target) { :practiceds }
+            it '1:Nとなっている' do
+                expect(association.macro).to eq :has_many
+            end
+        end
+
+        context 'Recordモデルとの関係' do
+            let(:target) { :records }
+            it '1:Nとなっている' do
+                expect(association.macro).to eq :has_many
+            end
+        end
+
+        context 'Commentモデルとの関係' do
+            let(:target) { :comments }
+            it '1:Nとなっている' do
+                expect(association.macro).to eq :has_many
+            end
+        end
+    end
+
+    describe 'データベースへの接続のテスト' do
+        subject { described_class.connection_config[:database] }
+    
+        it '指定のDBに接続していること' do
+            is_expected.to match(/monochords_test/)
+        end
+    end
 end
