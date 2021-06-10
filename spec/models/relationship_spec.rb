6db123346449a2ca8447ed1c812be05abc377ffc
JB0909 <j.kim@dct21.jp>
2021-06-10 19:04:36 +0900

    RSpec追加 -> フォロー・フォロワー機能

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Relationship, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'モデルとの関係' do
+    it 'following' do
+      expect(Relationship.reflect_on_association(:following).macro).to eq :belongs_to
+    end
+    
+    it 'follower' do
+      expect(Relationship.reflect_on_association(:follower).macro).to eq :belongs_to
+    end
+  end
 end
