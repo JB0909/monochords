9ade4639699763df43ab3e2a8c1920a6f2185851
JB0909 <j.kim@dct21.jp>
2021-06-10 20:05:37 +0900

    Rspec追加 => Music機能

--- spec/models/music_spec.rb
+++ spec/models/music_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Music, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'アソシエーションのテスト' do
+    let(:association) do
+        described_class.reflect_on_association(target)
+    end
+    
+    context 'Wishモデルとの関係' do
+      let(:target) { :wishes }
+      it '1:Nとなっている' do
+          expect(association.macro).to eq :has_many
+      end
+    end
+
+    context 'Practicingモデルとの関係' do
+      let(:target) { :practicings }
+      it '1:Nとなっている' do
+          expect(association.macro).to eq :has_many
+      end
+    end
+
+    context 'Practicedモデルとの関係' do
+      let(:target) { :practiceds }
+      it '1:Nとなっている' do
+          expect(association.macro).to eq :has_many
+      end
+    end
+  end
 end
