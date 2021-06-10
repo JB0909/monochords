0f77841e664af86ea2361ca3a0f7265162aa2d69
JB0909 <j.kim@dct21.jp>
2021-06-10 19:53:33 +0900

    RSpec追加 => レビュー機能

--- app/models/record.rb
+++ app/models/record.rb
@@ -5,6 +5,7 @@ class Record < ApplicationRecord
   #Like
   has_many :record_likes, dependent: :destroy
 
+  #バリデーション
   validates :user_id, presence: true
   validates :body, presence: true, length: {maximum: 255 }
   default_scope -> { order(created_at: :desc) }
