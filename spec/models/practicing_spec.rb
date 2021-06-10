62e3b72df330d3f111fb9b970a57fd645c1cf10f
JB0909 <j.kim@dct21.jp>
2021-06-10 19:59:43 +0900

    RSpec追加 => wish/practicing/practiced

--- spec/models/practicing_spec.rb
+++ spec/models/practicing_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Practicing, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'アソシエーションのテスト' do
+    let(:association) do
+        described_class.reflect_on_association(target)
+    end
+    
+    context 'Userモデルとの関係' do
+      let(:target) { :user }
+      it '1:Nとなっている' do
+          expect(association.macro).to eq :belongs_to
+      end
+    end
+
+    context 'Musicモデルとの関係' do
+      let(:target) { :music }
+      it '1:Nとなっている' do
+          expect(association.macro).to eq :belongs_to
+      end
+    end
+  end
 end
